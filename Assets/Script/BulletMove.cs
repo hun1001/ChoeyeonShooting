aaff9c6b40ef2d6f5ba11b2b101dc221540f07ca
l01022899266@gmail.com <l01022899266@gmail.com>
2021-07-22 11:29:06 +0900

    임시 배경 추가 밑 제네릭 풀 조금 작업

--- Assets/Script/BulletMove.cs
+++ Assets/Script/BulletMove.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pool;
+using System;
 
-public class BulletMove : MonoBehaviour
+public class BulletMove : MonoBehaviour, IResettable
 {
-    
+    public EventHandler Death;
     protected float speed = 1.57f;
+
+    public void Reset()
+    {
+        
+    }
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
