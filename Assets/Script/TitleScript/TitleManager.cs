a7824164af79e5cddc3c6ecd907e043822b74449
l01022899266@gmail.com <l01022899266@gmail.com>
2021-07-22 12:52:50 +0900

    타이틀 씬 구현 밑 파일 정리

---
+++ Assets/Script/TitleScript/TitleManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void RepairShop()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("ExitGame");
+    }
+}
