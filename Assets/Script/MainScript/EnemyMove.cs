b59af60bc570ed5923aef8502cae0853780c23e8
DESKTOP-OHD6DA9\USER <zmflqhaos123@gmail.com>
2021-07-23 10:45:39 +0900

    적 개체 2개 추가

--- Assets/Script/MainScript/EnemyMove.cs
+++ Assets/Script/MainScript/EnemyMove.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    [SerializeField]
+    protected float speed = 5f;
+    [SerializeField]
+    private int hp = 3;
+    private bool isDead = false;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
         CheckLimit();
     }
 
-    void CheckLimit()
+    protected void CheckLimit()
     {
         if (transform.position.y < GameManager.Instance.minPosition.y)
         {
             Destroy(gameObject);
         }
+        if (transform.position.x < GameManager.Instance.minPosition.x-2)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.x > GameManager.Instance.maxPosition.x+2)
+        {
+            Destroy(gameObject);
+        }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isDead) return;
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            BulletA bullet = collision.GetComponent<BulletA>();
+            if (bullet != null)
+            {
+                Destroy(bullet.gameObject);
+            }
+            hp--;
+            if (hp <= 0)
+            {
+                isDead = true;
+                Destroy(gameObject);
+            }
+        }
+    }
 }
